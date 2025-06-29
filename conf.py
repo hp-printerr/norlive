6fee144f6b5b56f7b414c4a2e9c85b696e314773
hp-printerr <106080415+hp-printerr@users.noreply.github.com>
2025-06-30 00:26:19 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'HP Smart'
+project = 'Norton Setup'
 
 
 # The full version, including alpha/beta/rc tags
